d7282888dc651bbed13f89c0a9911a800dc5eab6
Queenshiba <haruka0111.2a@gmail.com>
2022-03-30 20:34:32 +0900

    add num functions

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-console.log('test')
+let doublesPlayerNum = 20
+let doublesCourtNum = 5
+
+function getNumsPlayers(doublesPlayerNum) {
+    // get numbers of players
+    let doublesPlayerNumArr = [];
+    for (let i = 1; i <= doublesPlayerNum; i++) {
+        doublesPlayerNumArr.push(i);
+    }
+    return doublesPlayerNumArr;
+}
+
+
+function getNumsCourts(doublesCourtNum) {
+    // get amounts of courts
+    let doublesCourtNumArr = [];
+    for (let i = 1; i <= doublesCourtNum; i++) {
+        doublesCourtNumArr.push(i);
+    }
+    return doublesCourtNumArr;
+}
+
+function doublesPlayerNumGenerator() {
+    console.log(getNumsPlayers(doublesPlayerNum))
+    console.log(getNumsCourts(doublesCourtNum))
+    
+}
+// console.log(getNumsPlayers(doublesPlayerNum))
+// console.log(getNumsCourts(doublesCourtNum))
+
+// console.log(getNumsCourts(doublesCourtNum))
+doublesPlayerNumGenerator()
