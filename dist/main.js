fa1b6f80ebda2b337184ac1446fac272b1d28a3a
Queenshiba <haruka0111.2a@gmail.com>
2022-04-04 22:59:24 +0900

    add function second

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,8 @@ let doublesPlayerNum = 23
 let doublesCourtNum = 5
 let doublesGroupNum = 4
 
+let doublesNumsArr = []
+
 function getNumsPlayers(doublesPlayerNum) {
     // get numbers of players
     let doublesPlayerNumArr = [];
@@ -21,14 +23,31 @@ function getNumsCourts(doublesCourtNum) {
     return doublesCourtNumArr;
 }
 
+// let secondGroup
+
+function randamizePlayers(doublesPlayerNum, doublesGroupNum, doublesCourtNum) {
+    let secondArr
+    for (let i = 0; i < doublesPlayerNum.length; i++) {
+        secondArr = Array.from({ length: doublesCourtNum }, () => doublesPlayerNum.splice(i, doublesGroupNum),
+        doublesPlayerNum.splice(i+2, doublesGroupNum))
+        return secondArr
+    }
+
+
+}
+console.log(randamizePlayers(getNumsPlayers(doublesPlayerNum), doublesGroupNum, doublesCourtNum))
+
 function doublesPlayerGropsGenerator(doublesPlayerNum, doublesGroupNum, doublesCourtNum) {
     let gropsInArray
     if (doublesPlayerNum % doublesCourtNum) {
-    gropsInArray = Array.from({ length: doublesCourtNum}, () => doublesPlayerNum.splice(0, doublesGroupNum));
+        gropsInArray = Array.from({ length: doublesCourtNum }, () => doublesPlayerNum.splice(0, doublesGroupNum));
     } else {
-        gropsInArray = Array.from({ length: doublesCourtNum + 1}, () => doublesPlayerNum.splice(0, doublesGroupNum));
+        gropsInArray = Array.from({ length: doublesCourtNum + 1 }, () => doublesPlayerNum.splice(0, doublesGroupNum));
     }
     return gropsInArray
+
 }
 
-console.log(doublesPlayerGropsGenerator(getNumsPlayers(doublesPlayerNum), doublesGroupNum, doublesCourtNum))
+
+
+// console.log(doublesPlayerGropsGenerator(getNumsPlayers(doublesPlayerNum), doublesGroupNum, doublesCourtNum))
